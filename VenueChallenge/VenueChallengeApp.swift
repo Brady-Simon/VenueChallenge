fb4fc8c2b0d5b80de587a926965fbda2d54c5e70
BradySimon <bradytsimon@gmail.com>
2022-01-25 18:47:58 -0500

    Initial Commit

---
+++ VenueChallenge/VenueChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VenueChallengeApp.swift
+//  VenueChallenge
+//
+//  Created by Brady Simon on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct VenueChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
